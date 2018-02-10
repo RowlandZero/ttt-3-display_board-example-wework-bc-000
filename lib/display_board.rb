fedc28d12825f6bcb6832e96784bcd061aa74bfa
Steven Rowland <steven.rowland@wework.com>
2018-02-10 22:34:34 +0000

    automatically backed up by learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -1,6 +1,7 @@
 # Define a method display_board that prints a 3x3 Tic Tac Toe Board
-row_1 = "   |   |   "
-row_2 = "-----------"
-row_3 = "   |   |   "
-row_4 = "-----------"
-row_5 = "   |   |   "
+def display_board
+  row_1 = "   |   |   "
+  row_2 = "-----------"
+  row_3 = "   |   |   "
+  row_4 = "-----------"
+  row_5 = "   |   |   "
